98e1d23821cd7f4f8685ebcb0e82d55fcad667dc
Jose Pozo Silva <jhosede23@gmail.com>
2023-04-12 00:04:39 -0400

    fixing sintaxis on ElevatedButton, and changing Foreground to onPrimary

--- lib/ui/login_page.dart
+++ lib/ui/login_page.dart
@@ -30,10 +30,8 @@ class _LoginPageState extends State<LoginPage> {
         ),
       ),
       body: ListView(
-        
-        children: 
-          [Column(
-            
+        children: [
+          Column(
             mainAxisAlignment: MainAxisAlignment.center,
             // ignore: prefer_const_literals_to_create_immutables
             children: <Widget>[
@@ -50,25 +48,23 @@ class _LoginPageState extends State<LoginPage> {
                 padding: const EdgeInsets.all(8.0),
                 child: Text('Username'),
               ),
-              //textfield username 
+              //textfield username
               Container(
                 padding: EdgeInsets.all(10),
                 child: TextField(
                   //aceptar solo numeros y letras
                   inputFormatters: [
                     FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]')),
-                    
                   ],
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
                     icon: Icon(Icons.person),
-                    hintText: 'Username',                  
+                    hintText: 'Username',
                   ),
                 ),
               ),
               SizedBox(
                 height: 30,
-                
               ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -77,20 +73,20 @@ class _LoginPageState extends State<LoginPage> {
 
               //textfieldPassword
               Container(
-
                 padding: EdgeInsets.all(10),
                 child: TextField(
                   obscureText: _obscureText,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(),
-              
+
                     icon: Icon(Icons.security),
-              
+
                     hintText: 'Password',
                     //ver password
                     suffixIcon: IconButton(
-                      icon: Icon(
-                          _obscureText ? Icons.visibility : Icons.visibility_off),
+                      icon: Icon(_obscureText
+                          ? Icons.visibility
+                          : Icons.visibility_off),
                       onPressed: _toogleObscureText,
                       // Respond to button press
                     ),
@@ -101,11 +97,11 @@ class _LoginPageState extends State<LoginPage> {
               SizedBox(height: 10),
 
               Container(
-                
                 padding: EdgeInsets.all(10),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Color.fromARGB(255, 177, 16, 129),
+                    onPrimary: Colors.white,
+                    // foregroundColor: Colors.white, backgroundColor: Color.fromARGB(255, 177, 16, 129),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15.0),
                     ),
@@ -116,15 +112,14 @@ class _LoginPageState extends State<LoginPage> {
                   },
                   child: Text('Login', style: TextStyle(fontSize: 20)),
                 ),
-                
               ),
-
 
               Container(
                 padding: EdgeInsets.all(10),
                 child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                    foregroundColor: Colors.white, backgroundColor: Color.fromARGB(255, 177, 16, 129),
+                    onPrimary: Colors.white,
+                    // foregroundColor: Colors.white, backgroundColor: Color.fromARGB(255, 177, 16, 129),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(20.0),
                     ),
@@ -133,14 +128,11 @@ class _LoginPageState extends State<LoginPage> {
                     // Respond to button press
                     Navigator.pushNamed(context, "/");
                   },
-
                   child: Text('Cancelar', style: TextStyle(fontSize: 20)),
                 ),
               ),
 
-       
               SizedBox(height: 10),
-
             ],
           ),
         ],
