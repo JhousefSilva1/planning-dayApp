74c370a1da7fe037a4af7b2c1f2ad985d26a8db7
Jose Pozo Silva <jhosede23@gmail.com>
2023-04-16 13:12:28 -0400

    fixing windows 1

--- lib/cubit/home_task_page_cubit.dart
+++
@@ -1,8 +0,0 @@
-import 'package:bloc/bloc.dart';
-import 'package:equatable/equatable.dart';
-
-part '../screens/home_task_page_state.dart';
-
-class HomeTaskPageCubit extends Cubit<HomeTaskPageState> {
-  HomeTaskPageCubit() : super(HomeTaskPageInitial());
-}
