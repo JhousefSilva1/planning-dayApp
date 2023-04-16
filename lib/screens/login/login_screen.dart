dfe3f8164fc45aecb83f9da643171f5c10ced89c
Jose Pozo <jhosede23@gmail.com>
2023-04-16 17:42:02 -0400

    creating login files

---
+++ lib/screens/login/login_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tasks/cubit/authentication/authentication_cubit.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  late String _username;
+  late String _password;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Material(
+      child: Stack(
+        children: [
+          
+        ],
+      )
+    );
+  }
+}
