9b0e54d153720f44c18963bb877a36a5b1417bd4
Jose Pozo <jhosede23@gmail.com>
2023-04-18 13:59:44 -0400

    solvig CmakeList file

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,4 @@
-// // ignore_for_file: prefer_const_constructors
 
-// import 'package:flutter/material.dart';
-// import 'package:tasks/screens/login/login_screen.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Login Demo',
-//       home: Stack(
-//         children: [
-//           Container(
-//             decoration: const BoxDecoration(
-//               image: DecorationImage(
-//                 image: NetworkImage(''),
-//                 fit: BoxFit.cover,
-//               ),
-//             ),
-//           ),
-//           LoginScreen(),
-
-// ignore_for_file: use_key_in_widget_constructors
-
-//         ],
-//       ),
-//     );
-//   }
-// }
 import 'package:flutter/material.dart';
 import 'package:tasks/screens/login/login_screen.dart';
 import 'package:tasks/screens/task_panel/task_panel.dart';
