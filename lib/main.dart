8a47e344dbdcebe7a9c1c61825c0f522f7545433
Jose Armando Pozo Silva <50475377+JhousefSilva1@users.noreply.github.com>
2023-04-18 14:27:03 -0400

    Revert "Bloc cubit"

--- lib/main.dart
+++ lib/main.dart
@@ -1,60 +1,35 @@
-// // ignore_for_file: prefer_const_constructors
-
-// import 'package:flutter/material.dart';
-// import 'package:tasks/screens/login/login_screen.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Login Demo',
-//       home: Stack(
-//         children: [
-//           Container(
-//             decoration: const BoxDecoration(
-//               image: DecorationImage(
-//                 image: NetworkImage(''),
-//                 fit: BoxFit.cover,
-//               ),
-//             ),
-//           ),
-//           LoginScreen(),
-
-// ignore_for_file: use_key_in_widget_constructors
-
-//         ],
-//       ),
-//     );
-//   }
-// }
+// ignore_for_file: override_on_non_overriding_member
 import 'package:flutter/material.dart';
-import 'package:tasks/screens/login/login_screen.dart';
-import 'package:tasks/screens/task_panel/task_panel.dart';
+import 'package:tasks/ui/home_page.dart';
+import 'package:tasks/ui/home_tasks_page.dart';
+//import material dart file
 
-void main() {
-  runApp(MyApp());
-}
+//import material dart
 
-class MyApp extends StatelessWidget {
+
+import 'package:tasks/ui/login_page.dart';
+//impor
+
+void main ()=> runApp(MyApp());
+
+class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'My App',
+      title: 'Tasks',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.amber,
       ),
-      initialRoute: '/login',
+
+      initialRoute: "/",
       routes: {
-        '/login': (context) => LoginScreen(),
-        '/task_panel': (context) => TaskPanel(),
+        "/": (context) => HomePage(),
+        "/login":(context) => LoginPage(),
+        '/tasks': (context) => HomeTasksPage(),
+        
+        
       },
+     
     );
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import connectivity_plus
-import flutter_secure_storage_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
-  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
 }
